4054f859b4de51b05b34f8e8b18f664e19f753d1
Adrien Roches <adrien@roc-it.tech>
2024-12-06 12:03:45 +0100

    Update base url

--- assets/slidev/default-Cucr-v-2.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-C_1R1hF5.js";import{_ as r}from"../index-CsNyT2JL.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
